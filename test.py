dd5b6e9a4c0aa1acac00afcf98e4e40f5d35413c
Laurent Bachelier <laurent@bachelier.name>
2012-11-24 00:21:24 +0100

    Comments and a new test

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ from xunitparser import parse
 
 def test_read():
     with open('penguin-1352619250.xml') as f:
+        # the lib already does some sanity checks;
+        # passing this is already a good test in itself
         ts, tr = parse(f)
+
+        # check some expected results
         for test in ts:
             if test.basename == 'ArteTest':
                 assert test.good
@@ -16,3 +20,6 @@ def test_read():
                 assert not test.skipped
                 if test.methodname == 'test_post':
                     assert test.bad
+
+        # assert hashes are unique
+        assert len(list(hash(t) for t in ts)) == len(set(hash(t) for t in ts))
