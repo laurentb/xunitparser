2703cea446b2a75adcd1e39ebf45bc39b9538142
Laurent Bachelier <laurent@bachelier.name>
2012-11-24 00:16:13 +0100

    Better tests, silent

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 from xunitparser import parse
 
+
 def test_read():
     with open('penguin-1352619250.xml') as f:
         ts, tr = parse(f)
-        print tr
+        for test in ts:
+            if test.basename == 'ArteTest':
+                assert test.good
+                assert test.skipped
+            if test.basename == 'YouTubeTest':
+                assert test.good
+                assert not test.skipped
+            if test.basename == 'PastebinTest':
+                assert not test.skipped
+                if test.methodname == 'test_post':
+                    assert test.bad
