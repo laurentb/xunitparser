77e966eecb02323ced71b65381cd37a6d8a0e7b5
Laurent Bachelier <laurent@bachelier.name>
2012-12-01 06:26:47 +0100

    Release 1.1.0
    Bumping minor because of new attributes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='xunitparser',
-    version='1.0.1',
+    version='1.1.0',
     description='Read JUnit/XUnit XML files and map them to Python objects',
     long_description=open('README').read(),
     author='Laurent Bachelier',
