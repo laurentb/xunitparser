590f295aab1772b572105e68971823cf14f463a4
Laurent Bachelier <laurent@bachelier.name>
2013-06-21 02:08:27 +0200

    Support multiple results in a testcase again
    I have not seen it in the wild yet, and it probably still won't work on
    internal consistency tests; but at least the parsing is clean.

--- xunitparser.py
+++ xunitparser.py
@@ -162,18 +162,23 @@ class Parser(object):
                 tc = self.TC_CLASS(el.attrib['classname'], el.attrib['name'])
                 tc.seed('success', trace=el.text or None)
                 tc.time = to_timedelta(el.attrib.get('time'))
+                restags = False
                 for e in el:
                     if e.tag in ('failure', 'error', 'skipped'):
+                        tc = self.TC_CLASS(el.attrib['classname'], el.attrib['name'])
                         result = e.tag
                         typename = e.attrib.get('type')
                         message = e.attrib.get('message')
                         tc.seed(result, typename, message, e.text or None)
                         tc.time = to_timedelta(el.attrib.get('time'))
+                        ts.addTest(tc)
+                        restags = True
                     if e.tag == 'system-out':
                         tc.stdout = e.text.strip()
                     if e.tag == 'system-err':
                         tc.stderr = e.text.strip()
-                ts.addTest(tc)
+                if not restags:
+                    ts.addTest(tc)
 
 
 def parse(source):
